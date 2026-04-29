7de7830dce6bc7aca855d543ef0ea444a79b45e6
AkshayDilipThorat <101387734+AkshayDilipThorat@users.noreply.github.com>
2026-04-29 16:36:22 +0530

    Refactor error handling and logging in index.js

--- .github/action/hello/index.js
+++ .github/action/hello/index.js
@@ -1,27 +1,24 @@
-const core =require("@actions/core");
-const github =required("@actions/github");
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try{
-core.debug('Debug Message');
-core.warning("Warning message");
-core.error("Error message");
+try {
+  throw new Error("Some error message");
+  core.debug("Debug Message");
+  core.warning("Warning message");
+  core.error("Error message");
 
+  const name = core.getInput("who_to_greet");
 
-const name= core.getInput('who_to_greet');
-console.log('Hello ${name}');
+  console.log(`Hello ${name}`);
 
-const time=new Date();
-core.setOutput("time",time.toTimeString());
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
 
-core.exportVariable("HELLO_TIME",time);
+  core.exportVariable("HELLO_TIME", time);
 
-core.startGroup("Logging github context");
-console.log(JSON.strinify(github.context, null, 2));
-core.endGroup();
-
-
-}
-
-catch(error){
-core.setFailed(error.message)
+  core.startGroup("Logging github context");
+  console.log(JSON.stringify(github.context, null, 2));
+  core.endGroup();
+} catch (error) {
+  core.setFailed(error.message);
 }
